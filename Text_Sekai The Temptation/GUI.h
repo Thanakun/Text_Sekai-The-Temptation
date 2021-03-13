7351e560e6e7dcc66f7a29615093e35b7089e059
Thoop <76035444+Thanakun@users.noreply.github.com>
2021-03-13 22:36:04 +0700

    Textline update newline
    I just add another line to get newline

--- Text_Sekai The Temptation/GUI.h
+++ Text_Sekai The Temptation/GUI.h
@@ -5,7 +5,6 @@
 /*
 	Here, lay the game GUI.
 */
-
 #include<SFML/System.hpp>
 #include<SFML/Graphics.hpp>
 #include<SFML/Audio.hpp>
@@ -16,34 +15,38 @@
 #include<sstream>
 #include<string>
 
-
 class GUI {
 public:
 	//construtor and disstrutor
 	GUI();
 	virtual ~GUI();
 
-	//Update functions
+	//Updating functions
 	void Update();
 	void UpdateText1(std::string);
 	void UpdateText2(std::string);
-	/*void UpdateText3(std::string);
+	void UpdateText3(std::string);
 	void UpdateText4(std::string);
-	void UpdateText5(std::string);*/ 
-	
+	void UpdateText5(std::string);
+
 	//Rendering functions
 	void Render();
-	void RenderText1(sf::RenderTarget &T);
-	void RenderText2(sf::RenderTarget &H);
-	//void RenderText3(sf::RenderTarget &O);
-	//void RenderText4(sf::RenderTarget &O);
-	//void RenderText5(sf::RenderTarget &P);
-	
+	void RenderText1(sf::RenderTarget& T);
+	void RenderText2(sf::RenderTarget& H);
+	void RenderText3(sf::RenderTarget& O);
+	void RenderText4(sf::RenderTarget& O);
+	void RenderText5(sf::RenderTarget& P);
+
 	//Event
 	void pollEvent();
+	void InputText();
 
 	//Accessor
 	const bool Running() const;
+	bool ChecknGetInputStr(std::string&);
+
+	//Function
+	void ForceClose();
 
 
 private:
@@ -51,21 +54,21 @@ private:
 	sf::RenderWindow* window;
 	sf::Event Eve;
 	sf::VideoMode videomode;
-	
+
 	//Resouces
 	sf::Font font;
-	
 	//text declaring
 	sf::Text main_line1;
 	sf::Text main_line2;
 	sf::Text main_line3;
 	sf::Text main_line4;
 	sf::Text main_line5;
-	/*sf::Text main_*/
-	sf::String sentence;
-	//sf::String* holdersentence;
-	
-
+	//sf string for input
+	sf::String input;
+	//char array for getting user input
+	char holder[33];
+	//for store user input
+	std::string* Input_holder;
 
 	//function
 	void InitVar();
